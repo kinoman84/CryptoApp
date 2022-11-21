7093426f8844c52e36309893806d3b56b6323ceb
kinoman <a.buchnev.kinoman@gmail.com>
2022-11-21 22:35:54 +0300

    Добавил горизонтальную вёрстку главного экрана

--- app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.cryptoapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
 import com.example.cryptoapp.R
 import com.example.cryptoapp.presentation.coininfo.CoinDetailsFragment
 import com.example.cryptoapp.presentation.coinlist.CoinListFragment
@@ -11,10 +12,7 @@ class MainActivity : AppCompatActivity(), CoinListFragment.OnItemSelectedListene
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            //TODO добавить горизонтальную вёрстку с двумя фрагментами
-            openListFragment()
-        }
+        openListFragment()
     }
 
     override fun onItemSelect(coinId: Int) {
@@ -22,15 +20,33 @@ class MainActivity : AppCompatActivity(), CoinListFragment.OnItemSelectedListene
     }
 
     private fun openListFragment() {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container , CoinListFragment.newInstance())
-            .commit()
+        val containerId = if (isOnePane()) R.id.container else R.id.container_for_list
+        val listFragment = supportFragmentManager.findFragmentByTag("list_fragment")
+        if (listFragment != null) {
+            supportFragmentManager.beginTransaction()
+                .addToBackStack(null)
+                .remove(listFragment)
+                .replace(containerId, CoinListFragment.newInstance(), "list_fragment")
+                .commit()
+        } else {
+            supportFragmentManager.beginTransaction()
+                .addToBackStack(null)
+                .replace(containerId, CoinListFragment.newInstance(), "list_fragment")
+                .commit()
+        }
     }
 
     private fun openDetailsFragment(coinId: Int) {
+        val containerId = if (isOnePane()) R.id.container else R.id.container_for_details
+
         supportFragmentManager.beginTransaction()
             .addToBackStack(null)
-            .replace(R.id.container, CoinDetailsFragment.newInstance(coinId))
+            .replace(containerId, CoinDetailsFragment.newInstance(coinId))
             .commit()
+    }
+
+    private fun isOnePane() : Boolean {
+        val containerForDetails = findViewById<FragmentContainerView>(R.id.container_for_details)
+        return containerForDetails == null
     }
 }
